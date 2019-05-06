2d9b9827fdf5cb8ed78dca808de924297553ff38
Tri Nguyen <me@trinnguyen.com>
2019-05-06 12:40:53 +0200

    use hlint

--- h-99/h99.hs
+++ h-99/h99.hs
@@ -1,15 +1,15 @@
 -- | Extract the last element from a list, which must be finite and non-empty
 myLast :: [a] -> a
 myLast [] = errorWithoutStackTrace "Empty list"
-myLast (x:[]) = x
+myLast [x] = x
 myLast (_:xs) = myLast(xs)
 
 -- | Problem 2: find the last but one element of the list
 myButLast :: [a] -> a
 myButLast [] = errorWithoutStackTrace "Empty list"
-myButLast (x:[]) = errorWithoutStackTrace "Only one elem"
-myButLast (x:y:[]) = x
-myButLast (x:xs) = myButLast(xs)
+myButLast [_] = errorWithoutStackTrace "Only one elem"
+myButLast [x:_] = x
+myButLast (x:xs) = myButLast xs
 
 -- | Problem 3: Find the K'th element of the list (1-based list)
 elementAt :: [a] -> Int -> a
@@ -24,7 +24,7 @@ myLength (x:xs) = 1 + myLength(xs)
 
 -- | Prelude functions used
 myLength1 :: [a] -> Int
-myLength1 = sum . map (\_ -> 1)
+myLength1 = sum . map (const 1)
 
 -- | Problem 5: Reverse a list
 myReverse :: [a] -> [a]
@@ -36,8 +36,8 @@ myReverse (x:xs) = myReverse(xs) ++ [x]
 --- | Problem 6: Find out a listif a palin drome
 isPalindrome :: Ord a => [a] -> Bool
 isPalindrome [] = errorWithoutStackTrace "Empty list"
-isPalindrome (x:[]) = True
-isPalindrome (x:xs) = x < (head xs) && isPalindrome(xs)
+isPalindrome [x] = True
+isPalindrome (x:xs) = x < head xs && isPalindrome(xs)
 
 -- | Problem 7: Flatten a nested list structure
 -- ignore
@@ -46,13 +46,13 @@ isPalindrome (x:xs) = x < (head xs) && isPalindrome(xs)
 compress :: Eq a => [a] -> [a]
 compress a = compressSub a []
  where compressSub (x:xs) list = 
-        if elem x list
+        if x `elem` list
             then compressSub xs list  
             else compressSub xs (list ++ [x])
        compressSub [] list = list
 
 compress1 :: Eq a => [a] -> [a]
-compress1 (x:xs) = x : (compress $ dropWhile (== x) xs)
+compress1 (x:xs) = x : compress ( dropWhile (== x) xs)
 
 -- | Problem 9: Pack consecutive duplicates of list elements into sublists. If a list contains repeated elements they should be placed in separate sublists.
 -- pack ['a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e'] => ["aaaa","b","cc","aa","d","eeee"]
@@ -62,22 +62,82 @@ pack a = packSub a []
        packSub (x:xs) list = packSub xs (addToPack x list)
         where addToPack n [] = [[n]]
               addToPack n (lx:lxs) =
-               if (n == (head lx))
+               if n == head lx
                 then (n : lx) : lxs
-                else lx : (addToPack n lxs)
+                else lx : addToPack n lxs
 
 pack1 :: (Eq a) => [a] -> [[a]]
 pack1 [] = []
 pack1 [x] = [[x]]
-pack1 (x:xs) = if x `elem` (head (pack1 xs))
-              then (x:(head (pack1 xs))):(tail (pack1 xs))
-              else [x]:(pack1 xs)
+pack1 (x:xs) = if x `elem` head (pack1 xs)
+              then x:head (pack1 xs): tail (pack1 xs)
+              else [x]:pack1 xs
 
 -- | Problem 10: Run-length encoding of a list. Use the result of problem P09 to implement the so-called run-length encoding data compression method
 encode :: (Eq a) => [a] -> [(Int, a)]
 encode [] = []
 encode [x] = [(1, x)]
 encode (x:xs) = 
-    if x == (snd $ head $ encode xs)
-        then ((fst $ head $ encode xs) + 1, x) : (tail $ encode xs)
-        else (1, x) : (encode xs)
+    if x == snd $ head $ encode xs
+        then (fst (head $ encode xs) + 1, x) : tail $ encode xs
+        else (1, x) : encode xs
+
+data ListItem a = Mutiple Int a | Single a
+ deriving (Show)
+
+-- | Problem 11: Modified run-length encoding
+encodeModified :: (Eq a) => [a] -> [ListItem a]
+encodeModified = map encodeHelper . encode
+ where
+    encodeHelper (1,x) = Single x
+    encodeHelper (n, x) = Mutiple n x
+
+
+-- Problem 12: Decode a run-length encoded list
+decodeModified :: (Eq a) => [ListItem a] -> [a]
+decodeModified [] = []
+decodeModified (x:xs) = decodeHelper x ++ decodeModified xs
+ where
+    decodeHelper (Single x) = [x]
+    decodeHelper (Mutiple n x) = replicate n x 
+
+decodeModified' :: (Eq a) => [ListItem a] -> [a]
+decodeModified' = concatMap decodeHelper
+ where
+    decodeHelper (Single x) = [x]
+    decodeHelper (Mutiple n x) = replicate n x
+
+-- Problem 13: Run-length encoding of a list (direct solution). 
+
+-- | Problem 14: Duplicate the elements of the list
+dupli :: [a] -> [a]
+dupli = concatMap (\x -> [x,x])
+
+-- Raw solution
+dupli' :: [a] -> [a]
+dupli' [] = []
+dupli' (x:xs) = x : x : dupli' xs
+
+-- | Problem 15: Replicate the elements of a list a given number of times. 
+repli :: [a] -> Int -> [a]
+repli a n = concatMap (rephelper n) a
+ where 
+    rephelper 1 x = [x]
+    rephelper n x = x : rephelper (n -1) x 
+
+-- Raw solution
+repli' :: [a] -> Int -> [a]
+repli' [] _ = []
+repli' (x:xs) n = rephelper n x ++ repli' xs n
+ where
+    rephelper 1 x = [x]
+    rephelper n x = x : rephelper (n -1) x 
+
+-- | Problem 16: Drop every N'th element from a list. 
+dropEvery :: [a] -> Int -> [a]
+dropEvery a n = dropHelper a n 1
+    where
+        dropHelper [] _ _ = []
+        dropHelper (x:xs) n k
+            | k == n = dropHelper xs n 1
+            | otherwise = x : dropHelper xs n (k + 1)
