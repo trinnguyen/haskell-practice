abdd41d9a0c324e42d524521f2809f9e299e7b7c
Tri Nguyen <me@trinnguyen.com>
2019-05-06 13:13:50 +0200

    fix after using linter

--- h-99/h99.hs
+++ h-99/h99.hs
@@ -2,13 +2,13 @@
 myLast :: [a] -> a
 myLast [] = errorWithoutStackTrace "Empty list"
 myLast [x] = x
-myLast (_:xs) = myLast(xs)
+myLast (_:xs) = myLast xs
 
 -- | Problem 2: find the last but one element of the list
 myButLast :: [a] -> a
 myButLast [] = errorWithoutStackTrace "Empty list"
 myButLast [_] = errorWithoutStackTrace "Only one elem"
-myButLast [x:_] = x
+myButLast (x:y:[]) = x
 myButLast (x:xs) = myButLast xs
 
 -- | Problem 3: Find the K'th element of the list (1-based list)
@@ -20,7 +20,7 @@ elementAt (x:xs) k = elementAt xs (k - 1)
 -- | Problem 4: Find the the number of elements
 myLength :: [a] -> Int
 myLength [] = 0
-myLength (x:xs) = 1 + myLength(xs)
+myLength (x:xs) = 1 + myLength xs
 
 -- | Prelude functions used
 myLength1 :: [a] -> Int
@@ -30,14 +30,14 @@ myLength1 = sum . map (const 1)
 myReverse :: [a] -> [a]
 myReverse [] = []
 myReverse [x] = [x]
-myReverse (x:xs) = myReverse(xs) ++ [x]
+myReverse (x:xs) = myReverse xs ++ [x]
 
 
 --- | Problem 6: Find out a listif a palin drome
 isPalindrome :: Ord a => [a] -> Bool
 isPalindrome [] = errorWithoutStackTrace "Empty list"
 isPalindrome [x] = True
-isPalindrome (x:xs) = x < head xs && isPalindrome(xs)
+isPalindrome (x:xs) = x < head xs && isPalindrome xs
 
 -- | Problem 7: Flatten a nested list structure
 -- ignore
@@ -70,17 +70,17 @@ pack1 :: (Eq a) => [a] -> [[a]]
 pack1 [] = []
 pack1 [x] = [[x]]
 pack1 (x:xs) = if x `elem` head (pack1 xs)
-              then x:head (pack1 xs): tail (pack1 xs)
-              else [x]:pack1 xs
+              then (x:(head (pack1 xs))) : (tail (pack1 xs))
+              else [x]:(pack1 xs)
 
 -- | Problem 10: Run-length encoding of a list. Use the result of problem P09 to implement the so-called run-length encoding data compression method
 encode :: (Eq a) => [a] -> [(Int, a)]
 encode [] = []
 encode [x] = [(1, x)]
 encode (x:xs) = 
-    if x == snd $ head $ encode xs
-        then (fst (head $ encode xs) + 1, x) : tail $ encode xs
-        else (1, x) : encode xs
+    if x == snd (head $ encode xs)
+        then (fst (head (encode xs)) + 1, x) : tail (encode xs)
+        else encode [x] ++ encode xs
 
 data ListItem a = Mutiple Int a | Single a
  deriving (Show)
